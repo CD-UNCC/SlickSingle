51aec7945d5da98275dc5534250fcd22360221a1
cocoamuffs <linkmabus@hotmail.com>
2014-12-11 13:46:48 -0600

    Create Factory.java

---
+++ factory/Factory.java
@@ -0,0 +1,26 @@
+package factory;
+
+import entities.Item;
+import entities.Mob;
+import entities.Resource;
+import entities.players.Player;
+
+public class Factory {
+	
+	public Player newPlayer(){
+		return null;
+	}
+	
+	public Mob newMob(){
+		return null;
+	}
+	
+	public Resource newResource(float x, float y, int ID, int subID){
+		return null;
+	}
+	
+	public Item newItem(){
+		
+		return null;
+	}
+}
