8e7432f24f40f5fbcfa368354cce2fa8c8ca859f
cocoamuffs <linkmabus@hotmail.com>
2014-12-11 13:46:03 -0600

    Create Tree.java

---
+++ entities.resources/Tree.java
@@ -0,0 +1,12 @@
+package entities.resources;
+
+import entities.Resource;
+
+public class Tree extends Resource{
+
+	public Tree(float x, float y) {
+		super(x, y, ID);
+		// TODO Auto-generated constructor stub
+	}
+
+}
