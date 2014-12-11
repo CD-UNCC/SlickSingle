0f07d0dc3174745c4c207d84774fd485a42c28db
cocoamuffs <linkmabus@hotmail.com>
2014-12-11 13:44:19 -0600

    Create Fish.java

---
+++ entities.resources/Fish.java
@@ -0,0 +1,12 @@
+package entities.resources;
+
+import entities.Resource;
+
+public class Fish extends Resource{
+
+	public Fish(float x, float y) {
+		super(x, y, ID);
+		// TODO Auto-generated constructor stub
+	}
+
+}
