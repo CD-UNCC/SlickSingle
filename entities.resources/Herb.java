a5e912cd7587a4b968c1e47dd5aeee2921c19669
cocoamuffs <linkmabus@hotmail.com>
2014-12-11 13:45:30 -0600

    Create Herb.java

---
+++ entities.resources/Herb.java
@@ -0,0 +1,15 @@
+package entities.resources;
+
+import entities.Resource;
+
+public class Herb extends Resource{
+
+	public Herb(float x, float y) {
+		super(x, y, ID);
+		// TODO Auto-generated constructor stub
+	}
+
+  public int getID(){
+    return 3;
+  }
+}
